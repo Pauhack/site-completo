c41fd176edf293ae65b3275030682a0f3ac0f0dd
Paulo H <paulohildebrandt93@gmail.com>
2020-01-11 19:41:22 -0500

    primer commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <top-header/>
+    <main id="main" role="main">
+      <nuxt/>
+    </main>
+    <bottom-footer/>
+  </div>
+</template>
+
+<script>
+import TopHeader from '~/components/TopHeader.vue'
+import BottomFooter from '~/components/BottomFooter.vue'
+
+export default {
+  components: {
+    TopHeader,
+    BottomFooter
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/styles.scss';
+</style>
